d131fccc2eb6d38dbb5bc6e6f60e6e8457e13143
sk3r <skyer.sma@gmail.com>
2016-09-26 12:40:11 +0200

    prepare_docker_node installs docker-compose

--- prepare_docker_node.sh
+++ prepare_docker_node.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
 name=Docker Repository
@@ -7,15 +9,21 @@ gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
+# install docker
 sudo yum -y install docker-engine
 sudo systemctl start docker 
+
+# install docker-compose
+# FIXME might want to install it with pip later
+curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # this machine (docker node) will have some shared files in /root/shared
 # we want these to be available to every docker container
 # the folder ./fc-ci/distro_tests will be mounted inside the contaienrs, so we
 # copy the shared files there
-cp -r /root/shared/ ~/fc-ci/distro_tests
+cp -r /root/shared/* ~/fc-ci/distro_tests
 
 cd ~/fc-ci
-sudo docker-compose up
+sudo /usr/local/bin/docker-compose up
 
