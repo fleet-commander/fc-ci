62efc0fd91fd010b08b0947c4c2773b9fa45bd62
sk3r <skyer.sma@gmail.com>
2016-10-12 12:28:37 +0200

    rawhide.sh updates

--- distro_tests/rawhide.sh
+++ distro_tests/rawhide.sh
@@ -6,5 +6,15 @@ cat /root/shared/libvirt_node_ip
 dnf -y install git
 git clone https://github.com/fleet-commander/fc-admin
 cd fc-admin
-echo "do some tests, build admin, whatever"
-ls
+
+cd ~
+dnf -y install npm tar bzip2
+
+echo "installing node.js, phantomjs, and casperjs"
+npm install node.js
+npm install phantomjs-prebuilt
+npm install casperjs
+
+ls -la
+cd ./node_modules/
+ls -la
